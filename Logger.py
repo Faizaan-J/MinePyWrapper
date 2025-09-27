1ec16137f5e758c6cd1c8d96b771a1293c2f8dd6
Faizaan-J <125227430+Faizaan-J@users.noreply.github.com>
2025-09-27 18:55:32 -0400

    Add caller info to log output
    The logger now shoes the file and the line number that it was called from so that it's easier to trace.

--- Logger.py
+++ Logger.py
@@ -1,6 +1,8 @@
 import datetime
 from enum import Enum
 
+import inspect
+
 prefix = "MinePyWrapper"
 
 class LogLevel(Enum):
@@ -9,9 +11,14 @@ class LogLevel(Enum):
     ERROR = "ERROR"
 
 def Log(level: LogLevel, *args):
+    stack = inspect.stack()
+    caller = stack[1]
+    filename = caller.filename.split("/")[-1].split("\\")[-1]
+    line_number = caller.lineno
+
     now = datetime.datetime.now()
     timestamp = now.strftime("%H:%M:%S").zfill(8)
-    print(f"[{timestamp}] [{prefix}/{level.value}] {' '.join(str(arg) for arg in args)}")
+    print(f"[{timestamp}] [{prefix}/{level.value}] [{filename}:{line_number}] {' '.join(str(arg) for arg in args)}")
 
 # test
 Log(LogLevel.INFO, "Logger initialized", "Combined message test", 6, 7)
